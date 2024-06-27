166f5e5bf0f5c4e5025462405090ca480a301c4e
StealthM0de <jedongfallorina@gmail.com>
2024-06-27 16:22:29 +0800

    code update

--- validations/validation.js
+++ validations/validation.js
@@ -5,7 +5,11 @@ const contactValidation = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
-  favorite: Joi.boolean(),
 });
 
-export { contactValidation };
+// Define validation for updating favorite field
+const favoriteValidation = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+export { contactValidation, favoriteValidation };
