bca733f183ff19bb447b1235f5090ff1dcb8db46
StealthM0de <jedongfallorina@gmail.com>
2024-07-01 16:01:37 +0800

    updateavatar

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
 // prettier-ignore
-import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription} from "../../controllers/usersController.js";
+import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar} from "../../controllers/usersController.js";
 import { authenticateToken } from "../../middlewares/authenticateToken.js";
+import { upload } from "../../middlewares/upload.js";
 
 const router = express.Router();
 
@@ -43,5 +44,4 @@ router.patch("/", authenticateToken, ctrlWrapper(updateUserSubscription));
 // prettier-ignore
 router.patch("/avatars", authenticateToken, upload.single("avatar"), ctrlWrapper(updateAvatar));
 
-router.patch("/", authenticateToken, ctrlWrapper(updateUserSubscription));
 export { router };
